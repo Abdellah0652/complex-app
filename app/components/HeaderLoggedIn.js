828de94092f9a49bdbf2bdc48f38e7f9d6708493
abdellah targaoui <abdellah.targaoui@gmail.com>
2022-09-30 18:31:55 +0100

    add use reducer on my project

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -1,27 +1,28 @@
 import React, { useEffect, useContext } from "react"
 import { Link } from "react-router-dom"
-import ExampleContext from "../ExampleContext"
+import DispatchContext from "../DispatchContext"
+import StateContext from "../StateContext"
+
 function HeaderLoggedIn(props) {
-  const { setLoggedIn } = useContext(ExampleContext)
+  const appDispatch = useContext(DispatchContext)
+  const appState = React.useContext(StateContext)
 
   function handleLogout() {
-    setLoggedIn(false)
-    localStorage.removeItem("complexappToken")
-    localStorage.removeItem("complexappUsername")
-    localStorage.removeItem("complexappAvatar")
+    appDispatch({ type: "logout" })
   }
+
   return (
     <div className="flex-row my-3 my-md-0">
-      <Link to="#" className="text-white mr-2 header-search-icon">
+      <a href="#" className="text-white mr-2 header-search-icon">
         <i className="fas fa-search"></i>
-      </Link>
+      </a>
       <span className="mr-2 header-chat-icon text-white">
         <i className="fas fa-comment"></i>
         <span className="chat-count-badge text-white"> </span>
       </span>
-      <Link to="#" className="mr-2">
-        <img className="small-header-avatar" src={localStorage.getItem("complexappAvatar")} />
-      </Link>
+      <a href="#" className="mr-2">
+        <img className="small-header-avatar" src={appState.user.avatar} />
+      </a>
       <Link className="btn btn-sm btn-success mr-2" to="/create-post">
         Create Post
       </Link>
